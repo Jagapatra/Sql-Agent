b688830321a425e4419ad12c21b0170e9fa3664f
Jagannath patra <127885365+Jagapatra@users.noreply.github.com>
2025-12-04 13:38:21 +0530

    added chat ui and server route

---
+++ app/api/chat/route.ts
@@ -0,0 +1,18 @@
+import { streamText, UIMessage, convertToModelMessages } from 'ai';
+
+export const maxDuration = 30;
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY!,
+});
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model:google('gemini-2.5-flash'),
+    messages: convertToModelMessages(messages),
+  });
+
+  return result.toUIMessageStreamResponse();
+}
